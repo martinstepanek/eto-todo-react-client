ca89600bce605e23acdec87842c1344622a13521
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-04 01:39:51 +0200

    feat: add base Heading component

---
+++ src/components/base/Heading.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-family: 'Nunito Sans', sans-serif;
+  letter-spacing: 2px;
+  font-size: 3em;
+  margin: 10px 0;
+  user-select: none;
+`;
+
+export { Heading };
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
+import colors from './colors';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap');
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
   body {
-    background-color: black;
-    color: white;
+    background-color: ${colors.background};
+    color: ${colors.text};
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
   }
 `;
 
