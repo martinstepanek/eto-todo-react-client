81f4766957379232ed1c27999c1f45c1af505959
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-23 02:02:29 +0200

    refactor: move some global styles to global style variables

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 import colors from './colors';
 
+export const styleVariables = {
+  fontFamily: `'Roboto', sans-serif`,
+  fontSize: `18px`,
+};
+
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap');
@@ -14,8 +19,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${colors.background};
     color: ${colors.text};
-    font-family: 'Roboto', sans-serif;
-    font-size: 18px;
+    font-family: ${styleVariables.fontFamily};
+    font-size: ${styleVariables.fontSize};
     padding: 0;
     margin: 0;
     box-sizing: border-box;
