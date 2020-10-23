d95b8b036576eddad018d9e26bf2192d4fdea3f2
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-24 01:20:32 +0200

    feat: add createTask action

--- src/views/home/create-task-form/CreateTaskForm.tsx
+++ src/views/home/create-task-form/CreateTaskForm.tsx
@@ -3,6 +3,9 @@ import TaskForm, { FormHandle, TaskFormValues } from '../task-form/TaskForm';
 import { DateType } from '../types/DateType';
 import CREATE_TASK from './createTask';
 import { useMutation } from '@apollo/client';
+import { TaskListType } from '../types/TaskListType';
+import GET_TASKS from '../task-list/getTasks';
+import { TaskOperation } from '../types/TaskOperation';
 
 interface CreateTaskFormProps {
   onSubmit: () => void;
@@ -20,7 +23,27 @@ const CreateTaskForm = forwardRef<FormHandle, CreateTaskFormProps>(
       recurrentDateValue: 0,
     };
 
-    const [createTask, { data }] = useMutation(CREATE_TASK);
+    const [createTask] = useMutation(CREATE_TASK, {
+      update(cache, { data: { createTask } }) {
+        const query = GET_TASKS;
+        const taskOperation = createTask as TaskOperation;
+
+        taskOperation.inLists.map((listType: TaskListType) => {
+          const data: any = cache.readQuery({
+            query,
+            variables: { listType },
+          });
+
+          cache.writeQuery({
+            query,
+            variables: { listType },
+            data: {
+              tasks: [...data.tasks, taskOperation.task],
+            },
+          });
+        });
+      },
+    });
 
     const onTaskFormSubmit = async (values: TaskFormValues) => {
       onSubmit();
