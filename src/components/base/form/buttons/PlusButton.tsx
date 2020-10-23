a601389d6531393f1af03fc9c79188fd2e4168ab
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-23 02:02:46 +0200

    feat: add icon into PlusButton

--- src/components/base/form/buttons/PlusButton.tsx
+++ src/components/base/form/buttons/PlusButton.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from 'react';
 import { Button } from './Button';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const PlusButton: FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = props => {
-  return <Button {...props}>+</Button>;
+  return (
+    <Button {...props}>
+      <FontAwesomeIcon icon={faPlus} />
+    </Button>
+  );
 };
 
 export default styled(PlusButton)`
