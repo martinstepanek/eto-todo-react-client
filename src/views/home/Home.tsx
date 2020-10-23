071eb1bb777389f7de75f908569fddff46ca5c4b
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-23 02:04:06 +0200

    feat: add swipeable TaskForm into Home

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,9 +1,61 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { TaskListType } from './task-list/TaskListType';
 import TaskListPage from './task-list/TaskListPage';
+import styled from 'styled-components';
+import PlusButton from '../../components/base/form/buttons/PlusButton';
+import SwipeableBottomSheet from 'react-swipeable-bottom-sheet';
+import TaskForm from './task-form/TaskForm';
+import colors from '../../styles/colors';
 
-const Home: FC = () => {
-  return <TaskListPage listType={TaskListType.Todo} />;
+const Home: FC = props => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const formRef = useRef<any>(null);
+
+  const onFormVisibilityChange = value => {
+    setIsFormOpen(value);
+    if (value) {
+      if (formRef && formRef.current) {
+        formRef.current?.open();
+      }
+    }
+  };
+
+  const onPlusButtonClick = () => {
+    onFormVisibilityChange(true);
+  };
+
+  return (
+    <div {...props}>
+      <TaskListPage listType={TaskListType.Todo} />
+      <PlusButton className="plus-button" onClick={onPlusButtonClick} />
+      <SwipeableBottomSheet
+        open={isFormOpen}
+        scrollTopAtClose={true}
+        onChange={onFormVisibilityChange}
+      >
+        <TaskForm ref={formRef} />
+      </SwipeableBottomSheet>
+    </div>
+  );
 };
 
-export default Home;
+export default styled(Home)`
+  position: relative;
+  overflow: auto;
+  height: 100%;
+
+  .plus-button {
+    position: absolute;
+    bottom: 100px;
+    right: 20px;
+  }
+
+  .react-swipeable-view-container,
+  .react-swipeable-view-container > div,
+  .react-swipeable-view-container > div > div {
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    background-color: ${colors.background} !important;
+  }
+`;
