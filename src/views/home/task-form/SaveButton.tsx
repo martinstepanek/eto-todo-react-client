8b058e69bfd76916ecf755abd2d81989ca8ff2ed
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-23 02:02:59 +0200

    feat: add SaveButton

---
+++ src/views/home/task-form/SaveButton.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Button } from '../../../components/base/form/buttons/Button';
+import colors from '../../../styles/colors';
+
+const SaveButton: FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = props => {
+  return <Button {...props}>Save</Button>;
+};
+
+export default styled(SaveButton)`
+  background-color: ${colors.textBackground};
+  color: ${colors.primary};
+
+  &:disabled {
+    background-color: ${colors.textBackground};
+    border: 0;
+    color: ${colors.mutedText};
+  }
+`;
